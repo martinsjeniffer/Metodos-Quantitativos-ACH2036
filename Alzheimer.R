84f09d1fb14bd4044cfd27ff5b1f6227677c783b
Fabriciolk <fabriciokashino@live.com>
2021-09-22 20:22:54 -0300

    codigo organizado em histogramas e testes, e adicionado codigo teste para frequencia acumulada

--- Alzheimer.R
+++ Alzheimer.R
@@ -3,6 +3,7 @@
 #install.packages('car')
 # install.packages("cowplot")
 library(cowplot)
+library(nortest)
 #install.packages("corrplot")
 #install.packages("caret")
 #install.packages("caretEnsemble")
@@ -23,11 +24,11 @@ df <- read.csv('oasis_longitudinal.csv')
 head(df)
 #df
 
-dim(df)
-print('/n')
-str(df)
-print('/n')
-summary(df)
+# dim(df)
+# print('/n')
+# str(df)
+# print('/n')
+# summary(df)
 
 #Drop hand
 table(df$Hand)
@@ -49,34 +50,67 @@ head(df)
 #df %>% group_by(MMSE) %>% arrange(Group) 
 count(df, 'Group')
 
-df1 <- df[order(df$Group),][c(0:37),c(1,11,12)]
-#df1
-df2 <- df[order(df$Group),][c(38:183),c(1,11,12)]
-#df2
-df3 <- df[order(df$Group),][c(184:373),c(1,11,12)]
-#df3
+###############################################################
+############## DATAFRAMES SEPARADOS EM GRUPOS #################
+###############################################################
 
-hist(df1$ASF)
-hist(df2$ASF)
-hist(df3$ASF)
+demented_and_converted_df <- df[order(df$Group),][c(0:183),c(1,10,11,12)]
+non_demented_df <- df[order(df$Group),][c(184:373),c(1,10,11,12)]
 
-shapiro.test(df1$ASF)
-shapiro.test(df2$ASF)
-shapiro.test(df3$ASF)
+###############################################################
+##################### SHAPIRO TESTE ###########################
+###############################################################
 
-hist(df1$nWBV)
-hist(df2$nWBV)
-hist(df3$nWBV)
+shapiro.test(demented_and_converted_df$ASF)
+shapiro.test(non_demented_df$ASF)
 
-shapiro.test(df1$nWBV)
-shapiro.test(df2$nWBV)
-shapiro.test(df3$nWBV)
+shapiro.test(demented_and_converted_df$nWBV)
+shapiro.test(non_demented_df$nWBV)
 
-ggplot(df, aes(Group, EDUC))+
-  geom_boxplot(col = "blue")+
-  geom_point(stat = "summary", fun.y = "mean", col = "red", size = 4)+
-  ggtitle("Education and Dementia")+
-  theme(plot.title = element_text(hjust = .5))
+shapiro.test(demented_and_converted_df$eTIV)
+shapiro.test(non_demented_df$eTIV)
+
+################################################################
+##################### HISTOGRAMAS ##############################
+################################################################
+
+hist(demented_and_converted_df$ASF)
+hist(non_demented_df$ASF)
+
+hist(demented_and_converted_df$nWBV)
+hist(non_demented_df$nWBV)
+
+hist(demented_and_converted_df$eTIV)
+hist(non_demented_df$eTIV)
+
+################################################################
+##################### LILLIE TESTE #############################
+################################################################
+
+lillie.test(demented_and_converted_df$ASF)
+lillie.test(non_demented_df$ASF)
+
+lillie.test(demented_and_converted_df$nWBV)
+lillie.test(non_demented_df$nWBV)
+
+lillie.test(demented_and_converted_df$eTIV)
+lillie.test(non_demented_df$eTIV)
+
+################################################################
+####################### BOXPLOTS ###############################
+################################################################
+
+boxplot(demented_and_converted_df$eTIV)
+boxplot(non_demented_df$eTIV)
+
+# print(lillie.test(df1$nWBV))
+# print(nrow(df1))
+
+# ggplot(df, aes(Group, EDUC))+
+#   geom_boxplot(col = "blue")+
+#   geom_point(stat = "summary", fun.y = "mean", col = "red", size = 4)+
+#   ggtitle("Education and Dementia")+
+#   theme(plot.title = element_text(hjust = .5))
 
 #No real difference
 #ggplot(MRI_data, aes(M.F, EDUC))+
@@ -84,10 +118,10 @@ ggplot(df, aes(Group, EDUC))+
 #  ggtitle("Years of Education by Sex")+
 #  theme(plot.title = element_text(hjust = .5))
 
-anova_one_way <- aov(nWBV~Group, data = df)
-summary(anova_one_way)
-
-anova(aov(ASF~Group, data = df))
+# anova_one_way <- aov(nWBV~Group, data = df)
+# summary(anova_one_way)
+# 
+# anova(aov(ASF~Group, data = df))
 
 ########################## Editado por Fabricio #############################
 ################## Plotagem de grafico para Lillie Test #####################
@@ -98,10 +132,22 @@ max_lista_valores_atributo <- max(unlist(lista_de_valores_do_atributo))
 media_lista_valores_atributo <- mean(lista_de_valores_do_atributo)
 desvio_padrao_lista_valores_atributo <- sd(lista_de_valores_do_atributo)
 
-windows.options(width=10, height=20)
+
 plot(seq(min_lista_valores_atributo,max_lista_valores_atributo,0.01), pnorm(seq(min_lista_valores_atributo,max_lista_valores_atributo,0.01), media_lista_valores_atributo, desvio_padrao_lista_valores_atributo, lower.tail = TRUE, log.p = FALSE),ylab = "probability", xlab = "mm", type = "l", lwd=2)
 
 plot(ggplot(data = data.frame(x = seq(min_lista_valores_atributo, max_lista_valores_atributo)), aes(x)) + stat_function(fun = pnorm, n = 101, args = list(mean = media_lista_valores_atributo, sd = desvio_padrao_lista_valores_atributo)) + ylab("aaa") + xlab("fwefew"))
 
 #############################################################################
 #############################################################################
+
+# vetor teste
+teste <- c(1,4,7,2,7,2,8,45,67,8,2,2,75,2,7,2,2)
+# vetor soma acumulada
+print(cumsum(teste))
+# frequencias relativas
+print(prop.table(teste))
+# frequencia acumulada
+print(cumsum(table(teste)))
+
+plot()
+
