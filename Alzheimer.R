4d24bc01573c73e845d8cc49a4555cf33a39560c
Fabriciolk <fabriciokashino@live.com>
2021-09-22 16:35:12 -0300

    added codigo-teste para plotar grafico para lillie test

--- Alzheimer.R
+++ Alzheimer.R
@@ -1,10 +1,10 @@
-install.packages("dplyr")
-install.packages('plyr')
-install.packages('car')
+#install.packages("dplyr")
+#install.packages('plyr')
+#install.packages('car')
 #install.packages("corrplot")
 #install.packages("caret")
 #install.packages("caretEnsemble")
-install.packages("ggplot2")
+#install.packages("ggplot2")
 #install.packages("randomForest")
 #install.packages("xgboost")
 library(dplyr)
@@ -86,3 +86,18 @@ anova_one_way <- aov(nWBV~Group, data = df)
 summary(anova_one_way)
 
 anova(aov(ASF~Group, data = df))
+
+########################## Editado por Fabricio #############################
+################## Plotagem de grafico para Lillie Test #####################
+
+lista_de_valores_do_atributo <- c(93.45, 94.46, 94.93, 96.17, 96.74, 97.07, 97.68, 97.93, 99.1, 99.3, 100.73, 103.29, 103.6, 103.83, 105.2)
+min_lista_valores_atributo <- min(unlist(lista_de_valores_do_atributo))
+max_lista_valores_atributo <- max(unlist(lista_de_valores_do_atributo))
+media_lista_valores_atributo <- mean(lista_de_valores_do_atributo)
+desvio_padrao_lista_valores_atributo <- sd(lista_de_valores_do_atributo)
+
+windows.options(width=10, height=20)
+plot(seq(min_lista_valores_atributo,max_lista_valores_atributo,0.01), pnorm(seq(min_lista_valores_atributo,max_lista_valores_atributo,0.01), media_lista_valores_atributo, desvio_padrao_lista_valores_atributo, lower.tail = TRUE, log.p = FALSE),ylab = "probability", xlab = "mm", type = "l", lwd=2)
+
+#############################################################################
+#############################################################################
